d666f9218670e4844b7d6d4ce6492b71b8cdb62c
Lex Talkington <lex@lextalkington.com>
2013-12-10 16:46:14 -0800

    Trying new socket plugin

--- www/js/index.js
+++ www/js/index.js
@@ -9,29 +9,39 @@ var tfSock = null;
 var firstRun = true;
 var firstRunWin = true;
 var pmpSocket = null;
-
+var localSocketIP = "http://10.0.2.2:8080";
+var pmpSocketIP = "172.16.22.18:5556";
 
 // These are the strings to send over the socket to the PMP middleware
-var commandStrings = 		[];
-commandStrings["up"] = 		"up-----";
-commandStrings["left"] = 	"left---";
-commandStrings["enter"] = 	"enter--";
-commandStrings["right"] = 	"right--";
-commandStrings["down"] = 	"down---";
+var commandStrings = [];
+commandStrings["up"] = "up-----";
+commandStrings["left"] = "left---";
+commandStrings["enter"] = "enter--";
+commandStrings["right"] = "right--";
+commandStrings["down"] = "down---";
 
 // ON WINDOW READY
 window.addEventListener('load', onWindowReady, false);
 
 function onWindowReady() {
-	document.body.addEventListener('touchmove', function(e) { 
+	document.body.addEventListener('touchmove', function(e) {
 		e.preventDefault();
-    }, false);
-    
+	}, false);
+
 	if (firstRunWin) {
 		firstRunWin = false;
 		document.addEventListener("deviceready", onDeviceReady, false);
 	}
-	
+}
+
+// LOAD SCRIPT FUNCTION FROM SOCKET TUTORIAL
+function loadScript(url, callback) {
+	var head = document.getElementsByTagName('head')[0];
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = url;
+	script.onload = callback;
+	head.appendChild(script);
 }
 
 // ON DEVICE READY
@@ -129,67 +139,74 @@ function handleDrag(ev) {
 // SET UP WEB SOCKET
 function setupSocket() {
 	tfSock.innerHTML = "";
-	/*
-	tfSock.innerHTML = "setupSocket()";
-	
-	pmpSocket = new WebSocket('ws://172.16.22.18:5556');
 
-	pmpSocket.onopen = function() {
-		tfSock.innerHTML = "SOCKET OPEN";
-	};
+	loadScript('js/socket.io.js', function() {
 
-	pmpSocket.onclose = function() {
-		tfSock.innerHTML = "SOCKET CLOSED";
-		console.log('Connection closed');
-	};
+		pmpSocket = io.connect(pmpSocketIP);
 
-	pmpSocket.onerror = function(error) {
-		tfSock.innerHTML = "SOCKET ERROR: " + error;
-	};
+		tfSock.innerHTML = "connecting";
+		
+		pmpSocket.on('ping', function(data) {
+			tfSock.innerHTML = data.message;
+			/*
+			pmpSocket.emit('pong', {
+				message : 'Hello from client!'
+			});*/
+		});
 
-	pmpSocket.onmessage = function(msg) {
-		tfSock.innerHTML = "ON MSG: " + msg;
-	};
-	*/
+		pmpSocket.on('connect', function() {
+			tfSock.innerHTML = "connected";
+		});
+
+		pmpSocket.on('reconnect', function() {
+			tfSock.innerHTML = "reconnected";
+		});
+
+		pmpSocket.on('disconnect', function() {
+			tfSock.innerHTML = "disconnected";
+		});
+
+		pmpSocket.on('reconnecting', function() {
+			tfSock.innerHTML = "reconnecting...";
+		});
+
+		pmpSocket.on('error', function() {
+			tfSock.innerHTML = "error";
+		});
+	});
+
 };
 
 // SEND BINARY TO SOCKET
 function sendToSocket(str) {
-	// var binary = toUTF8Array(str);
-	// pmpSocket.send(binary.buffer);	
+	var binary = toUTF8Array(str);
+	pmpSocket.send(binary.buffer);
 }
 
 // CONVERT STRING TO UTF8 ARRAY
 function toUTF8Array(str) {
-    var utf8 = [];
-    for (var i=0; i < str.length; i++) {
-        var charcode = str.charCodeAt(i);
-        if (charcode < 0x80) utf8.push(charcode);
-        else if (charcode < 0x800) {
-            utf8.push(0xc0 | (charcode >> 6), 
-                      0x80 | (charcode & 0x3f));
-        }
-        else if (charcode < 0xd800 || charcode >= 0xe000) {
-            utf8.push(0xe0 | (charcode >> 12), 
-                      0x80 | ((charcode>>6) & 0x3f), 
-                      0x80 | (charcode & 0x3f));
-        }
-        // surrogate pair
-        else {
-            i++;
-            // UTF-16 encodes 0x10000-0x10FFFF by
-            // subtracting 0x10000 and splitting the
-            // 20 bits of 0x0-0xFFFFF into two halves
-            charcode = 0x10000 + (((charcode & 0x3ff)<<10)
-                      | (str.charCodeAt(i) & 0x3ff))
-            utf8.push(0xf0 | (charcode >>18), 
-                      0x80 | ((charcode>>12) & 0x3f), 
-                      0x80 | ((charcode>>6) & 0x3f), 
-                      0x80 | (charcode & 0x3f));
-        }
-    }
-    // add one last empty bit to the array
-    utf8.push(0x00);
-    
-    return utf8;
+	var utf8 = [];
+	for (var i = 0; i < str.length; i++) {
+		var charcode = str.charCodeAt(i);
+		if (charcode < 0x80)
+			utf8.push(charcode);
+		else if (charcode < 0x800) {
+			utf8.push(0xc0 | (charcode >> 6), 0x80 | (charcode & 0x3f));
+		} else if (charcode < 0xd800 || charcode >= 0xe000) {
+			utf8.push(0xe0 | (charcode >> 12), 0x80 | ((charcode >> 6) & 0x3f), 0x80 | (charcode & 0x3f));
+		}
+		// surrogate pair
+		else {
+			i++;
+			// UTF-16 encodes 0x10000-0x10FFFF by
+			// subtracting 0x10000 and splitting the
+			// 20 bits of 0x0-0xFFFFF into two halves
+			charcode = 0x10000 + (((charcode & 0x3ff) << 10) | (str.charCodeAt(i) & 0x3ff));
+			utf8.push(0xf0 | (charcode >> 18), 0x80 | ((charcode >> 12) & 0x3f), 0x80 | ((charcode >> 6) & 0x3f), 0x80 | (charcode & 0x3f));
+		}
+	}
+	// add one last empty bit to the array
+	utf8.push(0x00);
+
+	return utf8;
 };
